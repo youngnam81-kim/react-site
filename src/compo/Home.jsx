de00b76db099d2256149d5e105d825705fbe84ec
dongdong-byte <kimeee34@gmail.com>
2025-08-11 15:32:26 +0900

    Updates

--- src/compo/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return (
-        <div>
-            <h2>홈 페이지 MAIN입니다</h2>
-            {/* 홈 페이지 내용 추가 */}
-        </div>
-    );
-}
-export default Home;
--- src/compo/Login.jsx
+++
@@ -1,9 +0,0 @@
-function Login() {
-    return (
-        <div>
-            <h2>로그인 페이지 LOGIN</h2>
-            {/* 로그인 폼 컴포넌트 추가 */}
-        </div>
-    );
-}
-export default Login
