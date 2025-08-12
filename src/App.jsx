a3f6ccea7aaad679c7ce4db054602b04f0660a15
youngnam81-kim <kimyn81@naver.com>
2025-08-12 17:06:15 +0900

    0812

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import './App.css'
+import "./App.css";
 import Login from "./compo/Login.jsx";
 import Home from "./compo/Home.jsx";
 import Layout from "./compo/Layout.jsx";
 import Compo0811 from "./compo/0811/Compo0811.jsx";
 import Compo0812 from "./compo/0812/Compo0812.jsx";
 import Compo0808 from "./compo/0808/Compo0808.jsx";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+// import { HashRouter  as Router, Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>과제방</h1>
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route path="/react-site/" element={<Layout />} />
           <Route index element={<Home />} />
+          <Route path="/react-site/" element={<Layout />} />
           <Route path="/react-site/login" element={<Login />} />
           <Route path="/react-site/0811" element={<Compo0811 />} />
           <Route path="/react-site/0808" element={<Compo0808 />} />
           <Route path="/react-site/0812" element={<Compo0812 />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -48,7 +49,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
